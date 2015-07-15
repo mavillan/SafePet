8568250535af2796b8f40d6ff305d62f2b5b68f4
Rodrigo Cáceres <racacere@alumnos.inf.utfsm.cl>
2015-07-14 22:36:38 -0300

    Handle dogs data from the REST API, new item added to the sidebar

--- frontend/safepet/www/js/app.js
+++ frontend/safepet/www/js/app.js
@@ -1,38 +1,10 @@
-// Ionic Starter App
+angular.module('safePet', ['ionic','ngResource'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('safePet', ['ionic'])
-
-.controller('mainListController', ['$scope', '$ionicModal', function($scope,$ionicModal){
-  $scope.dogs = [
-    {
-      name: "Preta",
-      breed: "mestizo",
-      color: "negra",
-      photo: "https://runningforadream.files.wordpress.com/2012/05/perro_negro_foto.jpg"
-    },
-    {
-      name: "Boby",
-      breed: "labrador",
-      color: "Castaño",
-      photo: "http://labrador.reymascotas.com/imagenes/labradores/labrador-041.jpg"
-    },
-    {
-      name: "Cachupin",
-      breed: "mestizo",
-      color: "Negro con manchas castañas",
-      photo: "http://i.ytimg.com/vi/0EcP8Rjo4UQ/hqdefault.jpg"
-    },
-    {
-      name: "Laika",
-      breed: "mestizo",
-      color: "blanco con manchas negras",
-      photo: "http://3.bp.blogspot.com/-k3eVsZW9sDs/VF9-wqtLAYI/AAAAAAAAJ-E/o15l8TfFm0U/s1600/laika.jpg"
-    }
-
-  ];
+.controller('mainListController', ['$scope', '$ionicModal', '$resource', function($scope,$ionicModal,$resource){
+  
+  //Handle dogs information from API
+  Dog = $resource("http://safepetapi.labcomp.cl:5000/api/dogs/:id",{id: "@id"});
+  $scope.dogs = Dog.query();
 
   // Create and load the Modal
   $ionicModal.fromTemplateUrl('newDog.html', function(modal) {

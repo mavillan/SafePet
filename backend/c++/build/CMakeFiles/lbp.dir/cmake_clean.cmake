8408a9923542ccc1b7c9033f85b29b8e0153f5ef
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-08-19 22:44:56 -0300

    fighting with opencv

---
+++ backend/c++/build/CMakeFiles/lbp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lbp.dir/main.cpp.o"
+  "CMakeFiles/lbp.dir/lbp.cpp.o"
+  "CMakeFiles/lbp.dir/histogram.cpp.o"
+  "lbp.pdb"
+  "lbp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lbp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backend/c++/test.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv){
+	
+	if(argc!=2){
+		cout<<" Usage: display_image ImageToLoadAndDisplay"<<endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+
+	if(!image.data){
+		cout<<"Could not open or find the image"<<endl;
+		return -1;
+	}
+
+	namedWindow("Display window", WINDOW_NORMAL);
+	imshow( "Display window", image );
+	waitKey(0);  
+	return 0;
+
+}

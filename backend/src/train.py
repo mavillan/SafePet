751ace469a34da73405cae78de57711a7414d5af
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-08-21 19:11:30 -0300

    data_to_lbp() and data_to_sp_hist() done

--- backend/src/train.py
+++ backend/src/train.py
@@ -11,29 +11,47 @@ def data_to_lbp():
 	"""
 	> load images from TRAINING_PATH, and convert each one to
 	  lbp respresentation
-	> stores the result as .npy searialized array on LBP_PATH
+	> stores the results as .npy searialized array on LBP_PATH
 	"""
 	filenames=os.listdir(TRAINING_PATH)
-	for filename in files:
-		filename=TRAINING_PATH+filename
-		img=cv.imread(filename)
+	filenames.sort()
+	if len(filenames)==0:
+		sys.exit("TRAINING_PATH directory has no data!")
+	for filename in filenames:
+		target=TRAINING_PATH+filename #target name
+		img=cv.imread(target)
 		#To grayscale and to float
 		gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 		#LBP kernel convolution with the gray-scale image
 		#Applying LBPu2(P,R), no rotational invariant
 		lbp_image=lbp(gray,P,R,method=LBP_METHOD)
 		lbp_image=lbp_image.astype(np.uint8)
+		np.save(LBP_PATH+filename[:-4],lbp_image)
+	return 1
 
-	return
 
-
-def data_to_sp_hist():
+def data_to_sp_hist(nx,ny,numPatterns=59,overlapX=2,overlapY=2):
 	"""
 	> load images from LBP_PATH, and convert each one to
 	  spatial histogram respresentation
 	> stores the result as .npy searialized array on SP_HIST_PATH
 	"""
-	return
+	filenames=os.listdir(LBP_PATH)
+	filenames.sort()
+	if len(filenames)==0:
+		sys.exit("LBP_PATH directory has no data!")
+	#Generating empty target matrix
+	rows=len(filenames)
+	cols=59*nx*ny
+	row_index=0
+	overall_sp_hist=np.empty((rows,cols))
+	for filename in filenames:
+		target=LBP_PATH+filename #target name
+		lbp_image=np.load(target)
+		overall_sp_hist[row_index,:]=spatial_histogram(lbp_image,nx,ny)
+		row_index+=1
+	np.save(SP_HIST_PATH+"overall_sp_hist",overall_sp_hist)
+	return 1
 
 
 def _weight_calculate():
@@ -56,7 +74,7 @@ def _histogram(src,numPatterns=59):
 	return normalized_hist
 
 
-def spatial_histogram(src,nx,ny,numPatterns=59,overlapX=0,overlapY=0):
+def spatial_histogram(src,nx,ny,numPatterns=59,overlapX=2,overlapY=2):
 	"""
 	> the window size couldn't be fixed. Example, if there are 2 noses 
 	  pictures of the same dog, the first 800x800 and the second 400x400,
@@ -68,10 +86,10 @@ def spatial_histogram(src,nx,ny,numPatterns=59,overlapX=0,overlapY=0):
 	  axis, respectively.
 	"""
 	#some constrains to overlap parameter
-	if type(overlapX) is not int | overlapX<0 | overlapX>10:
+	if (type(overlapX) is not int) or (overlapX<0) or (overlapX>10):
 		print "Wrong overlapX parameter"
 		return -1
-	if type(overlapY) is not int | overlapY<0 | overlapY>10:
+	if (type(overlapY) is not int) or (overlapY<0) or (overlapY>10):
 		print "Wrong overlapY parameter"
 		return -1
 	height,width=src.shape

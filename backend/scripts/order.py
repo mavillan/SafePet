fd87bcb967b774de7beaff21f11723334bb4f152
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-08-26 15:57:45 -0300

    script to handle data

---
+++ backend/scripts/order.py
@@ -0,0 +1,37 @@
+#!/bin/python2
+import sys
+import os
+import subprocess
+
+"""
+WARNING: This scripts has some problems 
+when there exist name collitions. Not apply 
+over important data.
+"""
+
+
+if __name__ == '__main__':
+	if len(sys.argv)!=2:
+		sys.exit('Wrong input!')
+
+	path=sys.argv[1]
+	os.chdir(path)
+
+	cmd='convert {0} {1}'
+	ind=1 #starting index
+
+	for file_name in os.listdir("."):
+		new_id=str(ind)
+		new_id=(5-len(new_id))*'0'+new_id
+		target_name='dog_'+new_id+'.jpg'
+		if file_name.strip().split('.')[-1]!='jpg':
+			bashCommand=cmd.format(file_name,target_name)
+			process=subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+			process.communicate()[0]
+			os.remove(file_name)
+		else:
+			if file_name==target_name:
+				ind+=1
+				continue
+			os.rename(file_name,target_name)
+		ind+=1
